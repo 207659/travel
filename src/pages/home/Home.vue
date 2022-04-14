a325471ef564aa7444d62f0b079c2398cae0e51b
laiyunx <laiylm@foxmail.com>
2022-04-14 11:00:20 +0800

    city-keepalive

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -15,6 +15,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'home',
   components:{
@@ -22,15 +23,19 @@ export default {
   },
   data () {
     return {
+      lastCity:'',
       swiperList:[],
       iconList:[],
       recommendList:[],
       weekendList:[],
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   methods:{
     getHomeInfo () {
-      axios.get('/api/index.json').then(this.getHomeInfoOK)
+      axios.get('/api/index.json?city='+this.city).then(this.getHomeInfoOK)
     },
     getHomeInfoOK (res) {      
       res=res.data
@@ -44,7 +49,14 @@ export default {
     }
   },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
+  },
+  activated () {
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
+    }
   }
 }
 </script>
