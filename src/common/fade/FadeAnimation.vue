ee0e67959bf5e8305c70e142f97278456c15cb5d
laiyunx <laiylm@foxmail.com>
2022-04-16 14:44:00 +0800

    detail animation

---
+++ src/common/fade/FadeAnimation.vue
@@ -0,0 +1,35 @@
+<template>
+    <transition>
+        <slot></slot>
+    </transition>
+</template>
+  
+<script>
+export default {
+    name: 'FadeAnimation',
+    props: {
+        imgs: {
+            type: Array,
+            default () {
+                
+            }
+        }
+    },
+    data () {
+        return {
+            
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+  
+<style lang="stylus" scoped>
+.v-enter, .v-leave-to
+    opacity: 0
+.v-enter-active, .v-leave-active
+    transition: opacity .5s
+</style>
+  
--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -7,12 +7,15 @@
                 <div class="banner-number"><span class="iconfont banner-icon">&#xe67f;</span>{{this.gallaryImgs.length}}</div>
             </div>
         </div>
-        <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+        <fade-animation>
+            <common-gallary :imgs="gallaryImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+        </fade-animation>
     </div>    
 </template>
   
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
     name: 'DetailBanner',
     props: {
@@ -21,7 +24,7 @@ export default {
         gallaryImgs: Array,
     },
     components: {
-        CommonGallary
+        CommonGallary,FadeAnimation
     },
     data () {
         return {
